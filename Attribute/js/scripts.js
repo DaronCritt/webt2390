cb446c9fa36a94a5b152ca6c5d754d9b1d40665d
DaronCritt <ddcrittenden@my.chattanoogastate.edu>
2023-08-30 22:02:21 -0400

    updates

---
+++ Attribute/js/scripts.js
@@ -0,0 +1,33 @@
+$("button").click(function() {
+    $("ul").fadeToggle();
+});
+ 
+$("#dslide1").click(function () {
+    $("img").last().attr("src", "images/cross.jpg");
+});
+
+$("#dslide2").click(function () {
+    $("img").last().attr("src", "images/Diamond-eyed.jpg");
+});
+
+$("#dslide3").click(function () {
+    $("img").last().attr("src", "images/green_eyes.jpg");
+});
+
+$("#dslide4").click(function () {
+    $("img").last().attr("src", "images/closeup.jpg");
+});
+
+$("#dslide5").click(function () {
+    $("img").last().attr("src", "images/cat-vision-678x446.jpg");
+});
+
+$("#dslide6").click(function () {
+    $("img").last().attr("src", "images/bigeyes.png");
+});
+
+$(".main_cat1").attr("src", "images/cslide/cat4.jpg");
+
+$(".main_cat2").attr("src", "images/cslide/cat2.jpg");
+
+$(".main_cat3").attr("src", "images/cslide/catai5.jpg");
